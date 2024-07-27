11ccef5338ecbdc00eb5b788c4b2cbc13314ab0b
Alexander <alexandertsyrkin@gmail.com>
2024-07-27 23:38:48 +0400

    feat: fix header

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+   const burger = document.querySelector('.burger');
+   const nav = document.querySelector('.header-nav');
+
+   burger.addEventListener('click', () => {
+      nav.classList.toggle('active');
+   })
+});
